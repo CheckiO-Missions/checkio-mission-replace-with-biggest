17ccf48c1a51313deb63dc746e6523fa8f214f3a
Oleksandr Zozulia <freemanlex@gmail.com>
2022-11-09 16:30:25 +0200

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -11,26 +11,42 @@ Each test is a dict with
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": [[17, 18, 5, 4, 6, 1]],
+            "answer": [18, 6, 6, 6, 1, -1],
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+            "input": [[1, 2, 3, 4, 5, 6]],
+            "answer": [6, 6, 6, 6, 6, -1],
+        },
+        {
+            "input": [[1, 1, 1]],
+            "answer": [1, 1, -1],
+        },
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": [[6, 5, 4, 3, 2, 1, 0, -1]],
+            "answer": [5, 4, 3, 2, 1, 0, -1, -1],
         },
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
+            "input": [[-1, -1, -1]],
+            "answer": [-1, -1, -1],
+        },
+        {
+            "input": [[1, 0, 1, 0, 1, 0]],
+            "answer": [1, 1, 1, 1, 0, -1],
+        },
+        {
+            "input": [[0, 0, 0]],
+            "answer": [0, 0, -1],
+        },
+        {
+            "input": [[]],
+            "answer": [],
+        },
+        {
+            "input": [[1]],
+            "answer": [-1],
+        },
     ]
 }
